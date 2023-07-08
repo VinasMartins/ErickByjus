e1687f0a44d923de029fb7fb936129c94bfd31d9
ERyan <vpmartins28@gmail.com>
2023-07-08 12:41:32 -0300

    [Erick Ryan] fazendo o jogo invasao pirata - criando a bola do canhao

--- aulas/README.md
+++
@@ -1,3 +0,0 @@
-# Minhas aulas
-essa pasta serve para eu Erick colocar as aulas que eu pratico com a professora da plataforma byjus.
-[link para byjus](https://code.byjusfutureschool.com/s/dashboard)
--- aulas/aula-22/js/Cannon.js
+++ aulas/aula-22/js/Cannon.js
@@ -10,13 +10,6 @@ class Cannon {
     }
 
     display(){
-        if(keyIsDown(RIGHT_ARROW)&&this.angle <65){
-            this.angle = this.angle+1;
-        }
-
-        if(keyIsDown(LEFT_ARROW)&&this.angle >-50){
-            this.angle = this.angle-1;
-        }
         push ();
         translate (this.x,this.y);
         rotate (this.angle );
