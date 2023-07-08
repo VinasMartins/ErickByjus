5d516e45d9a022986e46f27c3cbe5a6dc4940adf
ERyan <vpmartins28@gmail.com>
2023-07-08 13:00:58 -0300

    [VPMartins] corrigindo projeto c21

--- projetos/projeto_c21/sketch.js
+++ projetos/projeto_c21/sketch.js
@@ -1,3 +1,17 @@
+var flag;
+
+var cyclist1,cyclist2,cyclist3,cyclist4,cyclist5,cyclist6;
+
+var finishgame;
+
+var playgame;
+
+var bicicle1,bicicle2,bicicle3,bicicle4,bicicle5,bicicle6;
+
+var gameState;
+
+var judge;
+
 function preload(){
 
 }
